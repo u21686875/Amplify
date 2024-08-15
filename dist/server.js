6761d3a6ec27f8bbf77d50e835aee1b5b84c5428
Thabang Kgaladi <99926326+u21686875@users.noreply.github.com>
2024-08-15 14:24:30 +0000

    Updated

---
+++ dist/server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express["static"](path.join(__dirname, 'public')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server is running on port ".concat(port));
+});
