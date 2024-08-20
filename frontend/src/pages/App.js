3b258182ed4e64cc74fa02cf1e0c91278d4b1d6b
Thabang Kgaladi <99926326+u21686875@users.noreply.github.com>
2024-08-20 11:07:22 +0000

    "Remove App.js, update index.js to import App from pages/App, and modify build script in package.json"

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -4,7 +4,7 @@ const App = () => {
     console.log('App component rendered');
     return (
         <div>
-            <h2>Welcome to Amplify</h2>
+            <h2>Welcome to Amplif</h2>
         </div>
     );
 };
