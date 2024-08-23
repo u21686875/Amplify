df1287b7d22bd0532e191c4eeae4f721b2306fc7
Thabang Kgaladi <u21686875@tuks.co.za>
2024-08-23 22:27:44 +0200

    feat: Add SplashPage component with custom font and styling

--- dist/server.js
+++ dist/server.js
@@ -5,12 +5,12 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory in the frontend folder
-app.use(express["static"](path.join(__dirname, '..', '..', 'frontend', 'public')));
+// Serve static files from the 'public' directory
+app.use(express["static"]('./public'));
 
 // Serve the main HTML file for all routes
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'public', 'index.html'));
+  res.sendFile('./public/index.html');
 });
 app.listen(PORT, function () {
   console.log("Server is running on http://localhost:".concat(PORT));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require("path");
-
 module.exports = {
-    entry: "./frontend/src/index.js",
+    entry: path.join(__dirname, 'frontend', 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'frontend', 'public'),
+        path: path.resolve("./frontend/public"),
         filename: "bundle.js"
     },
     mode: "development",
